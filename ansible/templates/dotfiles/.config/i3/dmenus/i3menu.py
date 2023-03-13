4ced3a5df19ec452474307746f7189bdc513d3c9
Serhii BOREMCHUK <serhii.boremchuk@smile-ukraine.com>
2023-03-13 08:24:55 +0200

    :tada: Initial commit
    Old templates, needs to be rewrote

---
+++ ansible/templates/dotfiles/.config/i3/dmenus/i3menu.py
@@ -0,0 +1,34 @@
+#!~/anaconda3/bin/python
+"""
+OnScreen menu for i3
+"""
+from pyrofi import QUICK, run_menu
+
+run_menu({
+    "border": {
+        "none": ["i3-msg", "border none"],
+        "1 pixel": ["i3-msg", "border 1pixel"],
+        "normal": ["i3-msg", "border normal"],
+        "title": ["i3-msg", "border normal 0"],
+    },
+    "run": {
+        "cmd": ["rofi", "-show", "run"],
+        "app": ["i3-dmenu-desktop" "--dmenu=\"rofi -dmenu -i -p App:\""]
+    },
+    "swap with": [
+        "sh", "-c",
+        "wmfocus -p | xargs -I {} i3-msg swap container with con_id {}",
+    ],
+    "mark": {
+        "mark ..": ["i3-input", "-F", "mark %s", "-l", "1", "-P", "Mark: "],
+        "go to ..": [
+            "i3-input", "-F", "[con_mark=%s] focus", "-l", "1", "-P", "Go to: "
+        ],
+    },
+    "move w/s to": {
+        "internal": ["i3-msg", "move workspace to output eDP-1"],
+        "VGA": ["i3-msg", "move workspace to output DP-2"],
+        "DP-1": ["i3-msg", "move workspace to output DP-1"],
+        "HDMI": ["i3-msg", "move workspace to output HDMI-1"]
+    }
+}, *QUICK, prefix="i3 ")
